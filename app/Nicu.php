b36018a0acc7c75c38c1e80f93a336edac53b303
agungsptr <agung.eka1999@gmail.com>
2019-10-18 19:39:42 +0000

    tambah function relastionship dan atur incrementing agar tidak error primaryKey-nonInteger

--- app/Nicu.php
+++ app/Nicu.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Penunjang;
 
 class Nicu extends Model
 {
     protected $table = "nicu";
     protected $primaryKey = "nicu_id";
     protected $timestamps = false;
+    public $incrementing = false;
+
+    public function penunjangs(){
+        return Penunjang::where('penunjang_nicu', $this->nicu_id)->get();
+    }
 }
--- app/RawatInap.php
+++ app/RawatInap.php
@@ -3,10 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Penunjang;
 
 class RawatInap extends Model
 {
     protected $table = "rawat_inap";
     protected $primaryKey = "raw_id";
     protected $timestamps = false;
+    public $incrementing = false;
+
+    public function penunjangs(){
+        return Penunjang::where('penunjang_ri', $this->raw_id)->get();
+    }
 }
