9782471a4efa64ba62eaee3e88ecdc230cca1095
agungsptr <agung.eka1999@gmail.com>
2019-11-28 14:56:25 +0000

    install spatie for backing up laravel app

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Spatie Backup
+        Spatie\Backup\BackupServiceProvider::class,
+
     ],
 
     /*
